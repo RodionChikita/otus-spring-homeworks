01576358ce2d0ea8baf94c5d927fd03f6de44bce
rodioncikita <chikita.rodion@gmail.com>
2024-06-19 03:09:24 +0300

    fix

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -7,7 +7,10 @@ import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.function.Function;
--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -2,6 +2,10 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
@@ -14,10 +18,12 @@ public class TestServiceImpl implements TestService {
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-        for (int i = 0; questionDao.findAll().size() > i; i++) {
-            ioService.printFormattedLine(i + 1 + ". %s", questionDao.findAll().get(i).text());
-            for (int j = 0; questionDao.findAll().get(i).answers().size() > j; j++) {
-                ioService.printFormattedLine("  " + (j + 1) + ") %s", questionDao.findAll().get(i).answers().get(j).text());
+        List<Question> questions = questionDao.findAll();
+        for (int i = 0; questions.size() > i; i++) {
+            ioService.printFormattedLine(i + 1 + ". %s", questions.get(i).text());
+            List<Answer> answers = questions.get(i).answers();
+            for (int j = 0; answers.size() > j; j++) {
+                ioService.printFormattedLine("  " + (j + 1) + ") %s", answers.get(j).text());
             }
         }
         // Получить вопросы из дао и вывести их с вариантами ответов
