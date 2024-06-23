934fa7c24c4b35f9d14e5e4d05aaa3c5ae497dd1
rodioncikita <chikita.rodion@gmail.com>
2024-06-23 20:25:50 +0300

    fix

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -2,33 +2,32 @@ package ru.otus.hw.dao;
 
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.function.Function;
 
 @RequiredArgsConstructor
-@Setter
 public class CsvQuestionDao implements QuestionDao {
     private static final Function<QuestionDto, Question> MAP_FROM_DTO_FUNCTION =
             q -> new Question(q.getText(), q.getAnswers());
 
     private final TestFileNameProvider fileNameProvider;
-    private BufferedReader reader;
 
     @Override
     public List<Question> findAll() {
         ClassLoader classLoader = getClass().getClassLoader();
         try (InputStream inputStream = classLoader.getResourceAsStream(fileNameProvider.getTestFileName());
              InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
-             ) {
-            setReader(new BufferedReader(streamReader));
+             BufferedReader reader = new BufferedReader(streamReader)) {
             var csvReader = new CsvToBeanBuilder<QuestionDto>(reader)
                     .withType(QuestionDto.class)
                     .withSeparator(';')
