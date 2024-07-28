814a9954136ae9fb0f2b61ff63c1b433ee9d205c
rodioncikita <chikita.rodion@gmail.com>
2024-07-28 14:04:57 +0200

    fix

--- hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw03-spring-boot/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -3,11 +3,9 @@ package ru.otus.hw.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import java.util.Locale;
 import java.util.Map;
 
-@ConfigurationPropertiesScan
 @Setter
 @ConfigurationProperties(prefix = "test")
 // Использовать @ConfigurationProperties.
